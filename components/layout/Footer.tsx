ed45502f225dcaeb59ba41db0f96821aeb03302c
Toni Jukica <tjcold@hotmail.com>
2020-02-17 19:51:21 +0100

    fix footer design

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,9 +3,7 @@ import { Container, Grid, makeStyles, createStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => createStyles({
     footer: {
-      backgroundColor: '#e1e4e8',
-      paddingTop: '32px',
-      paddingBottom: '32px'
+      borderTop: '1px solid #e1e4e8 !important'
     }
   }));
   
@@ -13,7 +11,7 @@ const useStyles = makeStyles(() => createStyles({
 const Footer: FunctionComponent = () => {
     const classes = useStyles();
     return(
-        <Container maxWidth = {false} className = {classes.footer}>
+        <Container maxWidth = 'xl' className = {classes.footer}>
             <Grid container direction = 'row' justify = 'center' alignItems = 'center'>
                 <span>Toni Jukica 2020</span>
             </Grid>
