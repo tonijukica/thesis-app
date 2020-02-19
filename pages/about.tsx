6e4f168c3e271a2136f572c96ee4c8954a1cbf14
Toni Jukica <tjcold@hotmail.com>
2020-02-19 19:01:09 +0100

    add StudentDetail component(basic)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Layout from '../components/layout/Layout'
+import StudentDetail from '..//components/StudentDetail';
 
 const AboutPage: React.FunctionComponent = () => (
   <Layout>
     <h1>About</h1>
     <p>This is the about page</p>
+    <StudentDetail></StudentDetail>
     <p>
       <Link href="/">
         <a>Go home</a>
