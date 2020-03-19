e06c886d298f1a862e60ff8dc725e6283c09ce1b
U-DESKTOP-58PJ9OE\tjcol <tjcold@hotmail.com>
2020-03-19 19:08:52 +0100

    hide navigation if not loggedin

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,60 +1,63 @@
-import { FunctionComponent } from 'react';
-import Link from 'next/link';
-import { Container, Grid, makeStyles, createStyles } from '@material-ui/core';
-import { useAuth0 } from '../../lib/auth0-spa';
+import { FunctionComponent } from "react";
+import Link from "next/link";
+import { Container, Grid, makeStyles, createStyles, Button } from "@material-ui/core";
+import { useAuth0 } from "../../lib/auth0-spa";
 
-const useStyles = makeStyles(() => createStyles({
-  header: {
-    backgroundColor: '#24292e',
-    paddingTop: '16px',
-    paddingBottom: '21px',
-    fontSize: '14px',
-    lineHeight: 1.5,
-    color: 'white'
-  },
-  row: {
-   height: '16px'
-  },
-  link: {
-    marginRight: '16px',
-    color: 'white'
-  }   
-}));
-
+const useStyles = makeStyles(() =>
+	createStyles({
+		header: {
+			backgroundColor: "#24292e",
+			paddingTop: "16px",
+			paddingBottom: "21px",
+			fontSize: "14px",
+			lineHeight: 1.5,
+      color: "white",
+      height: '64px'
+		},
+		row: {
+			height: "16px"
+		},
+		link: {
+			marginRight: "16px",
+			color: "white"
+		}
+	})
+);
 
 const Header: FunctionComponent = () => {
-  const classes = useStyles();
-  const { user, isAuthenticated, loginWithRedirect, logout, loading } = useAuth0();
-  return(
-    <Container maxWidth = {false} className = {classes.header}>
-        <Grid container direction = 'row' justify = 'space-around' alignItems = 'flex-start' className = {classes.row}>
-            <Grid item xs = {12} sm = {6}>
-                <div>
-                    APP
-                </div>
-            </Grid>
-            <Grid item xs = {12} sm = {6}>
-                <nav style = {{textAlign: 'end'}}>
-                    <Link href="/courses">
-                        <a className = {classes.link}>Courses</a>
-                    </Link>
-                    <Link href="/projects">
-                        <a className = {classes.link}>Projects</a>
-                    </Link>
-                    {!loading && !isAuthenticated && (
-                      <span onClick={() => loginWithRedirect({})}>Log in</span>
-                    )}
-                    { user &&
-                      <span className = {classes.link}>{user.nickname}</span>
-                    }
-                    {!loading && isAuthenticated && (
-                      <span onClick={() => logout()}>Log out</span>
-                    )}
-
-                </nav>
-            </Grid>
-        </Grid>
-    </Container>
-  )
-}
+	const classes = useStyles();
+	const { user, isAuthenticated, loginWithRedirect, logout, loading } = useAuth0();
+	return (
+		<Container maxWidth={false} className={classes.header}>
+			<Grid container direction='row' justify='space-around' alignItems='flex-start' className={classes.row}>
+				<Grid container item xs={12} sm={6} style={{height: '36.5px'}} alignContent='center'>
+          APP
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<nav style={{ textAlign: "end" }}>
+						{!loading && !isAuthenticated && (
+							<Button onClick={() => loginWithRedirect({})} color='primary'>
+								Log in
+							</Button>
+						)}
+						{!loading && isAuthenticated && (
+							<>
+								<Link href='/courses'>
+									<a className={classes.link}>Courses</a>
+								</Link>
+								<Link href='/projects'>
+									<a className={classes.link}>Projects</a>
+								</Link>
+								<span className={classes.link}>{user.nickname}</span>
+								<Button onClick={() => logout()} color='secondary'>
+									Log out
+								</Button>
+							</>
+						)}
+					</nav>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+};
 export default Header;
